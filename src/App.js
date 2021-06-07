075650e711e327d5242ff32c580545fdcae2111c
AysllaGomes <ayslla.gomes@outlook.com>
2021-06-07 19:37:24 -0300

    [UPT] Utilizando Links e NavLink, além do navigate para redirecionamento

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import Login from './Login';
 import Sobre from './Sobre';
 import Header from './Header';
 import NaoEncontrado from './NaoEncontrado';
@@ -12,7 +13,8 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/sobre" element={<Sobre />} />
+        <Route path="sobre" element={<Sobre />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<NaoEncontrado />} />
       </Routes>
     </BrowserRouter>
